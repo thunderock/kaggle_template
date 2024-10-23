baac9d11f74e012a88228e828f45e36d19c8134c
Ashutosh Tiwari <astiwari@adobe.com>
2024-10-22 21:22:29 -0700

    need to look at data

--- kaggle_template/scripts/download_dataset.py
+++ kaggle_template/scripts/download_dataset.py
@@ -8,17 +8,15 @@ import os
 if "snakemake" in sys.modules:
     COMPETITION_NAME = snakemake.params.competition
     ZIP_FILE_NAME = snakemake.output[0]
-# else:
-#     COMPETITION_NAME = "child-mind-institute-problematic-internet-use"
-#     ZIP_FILE_NAME = f"../data/raw/{COMPETITION_NAME}.zip"  # Fixed directory to match Snakefile
+else:
+    COMPETITION_NAME = "child-mind-institute-problematic-internet-use"
+    ZIP_FILE_NAME = f"../data/input/{COMPETITION_NAME}.zip"
 
 DOWNLOAD_DIR = os.path.dirname(ZIP_FILE_NAME)
 
-# Download competition data using Kaggle API
 kaggle.api.competition_download_files(
     competition=COMPETITION_NAME, path=DOWNLOAD_DIR, force=False, quiet=False
 )
 
-# Extract the downloaded ZIP file
 with zipfile.ZipFile(ZIP_FILE_NAME, "r") as zip_ref:
     zip_ref.extractall(DOWNLOAD_DIR)
