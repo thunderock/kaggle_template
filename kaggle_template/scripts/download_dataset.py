16a2020a86725c1ef8b6c3e7db8530f43f6c0fe7
Ashutosh Tiwari <astiwari@adobe.com>
2024-11-24 11:19:20 -0800

    changing dir and weide weight threshold

--- kaggle_template/scripts/download_dataset.py
+++ kaggle_template/scripts/download_dataset.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import zipfile
 
@@ -10,12 +11,16 @@ TRAIN_FILE = "data/input/train.csv"
 if "snakemake" in sys.modules:
     COMPETITION_NAME = snakemake.params.competition
     TRAIN_FILE = snakemake.output.train
+
 DOWNLOAD_DIR = os.path.dirname(TRAIN_FILE)
+if os.environ.get("KAGGLE_URL_BASE") is not None:
+    src = "/kaggle/input/child-mind-institute-problematic-internet-use/"
+    shutil.copy(src, DOWNLOAD_DIR)
+else:
+    kaggle.api.competition_download_files(
+        competition=COMPETITION_NAME, path=DOWNLOAD_DIR, force=False, quiet=False
+    )
 
-kaggle.api.competition_download_files(
-    competition=COMPETITION_NAME, path=DOWNLOAD_DIR, force=False, quiet=False
-)
-
-ZIP_FILE_NAME = os.path.join(DOWNLOAD_DIR, f"{COMPETITION_NAME}.zip")
-with zipfile.ZipFile(ZIP_FILE_NAME, "r") as zip_ref:
-    zip_ref.extractall(DOWNLOAD_DIR)
+    ZIP_FILE_NAME = os.path.join(DOWNLOAD_DIR, f"{COMPETITION_NAME}.zip")
+    with zipfile.ZipFile(ZIP_FILE_NAME, "r") as zip_ref:
+        zip_ref.extractall(DOWNLOAD_DIR)
